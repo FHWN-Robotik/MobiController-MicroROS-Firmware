9b4d88c620f5540ff4dafb2566b68584d44760d6
Flo2410 <florian@hye.dev>
2024-06-10 14:05:11 +0200

    Clean canlib.c

--- Core/Src/canlib.c
+++ Core/Src/canlib.c
@@ -37,7 +37,7 @@ HAL_StatusTypeDef canlib_send_extended(canlib_t *can, uint32_t ext_id, const uin
 }
 
 HAL_StatusTypeDef canlib_send_stop(canlib_t *can) {
-  uint8_t data[8] = {};
+  uint8_t data[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
   // Set is driving
   can->is_driving = false;
@@ -54,33 +54,14 @@ HAL_StatusTypeDef canlib_send_stop(canlib_t *can) {
  * @retval HAL status
  */
 HAL_StatusTypeDef canlib_drive(canlib_t *can, int16_t vx, int16_t vy, int16_t vphi) {
-  // vx = etl::clamp<int16_t>(vx, -3276, 3276);
-  // vy = etl::clamp<int16_t>(vy, -3276, 3276);
-  // vphi = etl::clamp<int16_t>(vphi, -3276, 3276);
-
-  // FIXME: the motorcontoller does wierd stuff above a value of 2077 mm/s
-  // vx = etl::clamp<int16_t>(vx, -2000, 2000);
-  // vy = etl::clamp<int16_t>(vy, -2000, 2000);
-  // vphi = etl::clamp<int16_t>(vphi, -2000, 2000);
-
+  // NOTE: The motorcontoller does wierd stuff above a value of 2077 mm/s
   vx = clamp(vx, -2000, 2000);
   vy = clamp(vy, -2000, 2000);
   vphi = clamp(vphi, -2000, 2000);
 
-  int16_t val_x = vx * 10; // m/s * 10000 -> mm/s * 10
-  uint16_t x_twos = twos_complement(val_x);
-  // uint8_t x_high = x_twos >> 8;
-  // uint8_t x_low = (uint8_t)x_twos;
-
-  int16_t val_y = vy * 10; // m/s * 10000 -> mm/s * 10
-  uint16_t y_twos = twos_complement(val_y);
-  // uint8_t y_high = y_twos >> 8;
-  // uint8_t y_low = (uint8_t)y_twos;
-
+  int16_t val_x = vx * 10;     // m/s * 10000 -> mm/s * 10
+  int16_t val_y = vy * 10;     // m/s * 10000 -> mm/s * 10
   int16_t val_phi = vphi * 10; // rad/s * 10000 -> mrad/s * 10
-  uint16_t phi_twos = twos_complement(val_phi);
-  // uint8_t phi_high = phi_twos >> 8;
-  // uint8_t phi_low = (uint8_t)phi_twos;
 
   uint8_t x_low = val_x & 0xFF;         // Extract the low byte of val_x
   uint8_t x_high = (val_x >> 8) & 0xFF; // Extract the high byte of val_x
@@ -103,5 +84,6 @@ HAL_StatusTypeDef canlib_drive(canlib_t *can, int16_t vx, int16_t vy, int16_t vp
   }
   can->last_update = rmw_uros_epoch_millis();
 
+  // Send CAN frame
   return canlib_send_extended(can, CANLIB_MOVE_LATERALLY, data);
 }
